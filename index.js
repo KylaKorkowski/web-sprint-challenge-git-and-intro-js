091b3fce0f7f66bc0779e0f30c49852bd7c45666
Kyla Korkowski <kekorkowski@gmail.com>
2022-10-17 20:21:15 -1000

    task 8 complete

--- index.js
+++ index.js
@@ -342,10 +342,17 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(array){
-  return (array.bio)
+function artistInfo(array, name){
+  let artistBio = '';
+  array.map(element => {
+    if(element.name === name) {
+      artistBio = element.bio;
+    }});
+
+    return artistBio;
 }
 
+console.log(artistInfo(artists, 'Frida Kahlo'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -358,11 +365,19 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, nationality){
+  // const filteredArr = [];
+  
+  // for(let i = 0; i < array.length; i++) {
+  //   if(array[i].region === nationality) {
+  //     filteredArr.push(array[i]);
+  //   }
+  // }
+
+  // return filteredArr;
 }
 
-
+console.log(artistByCountry())
 
 /* ***** END OF TASKS ***** */
 
